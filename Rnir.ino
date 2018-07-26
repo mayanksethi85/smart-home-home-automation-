fb4c8e04391e4e01157f2cc2c919a53d5359e624
mayanksethi85 <37772116+mayanksethi85@users.noreply.github.com>
2018-07-27 01:36:59 +0530

    Add files via upload

---
+++ Rnir.ino
@@ -0,0 +1,51 @@
+int ir1=8;
+int ir2=7;
+int led=3;
+int count=0;
+
+void setup() {
+  pinMode(ir1,INPUT);
+  pinMode(ir2,INPUT);
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+}
+void loop() {
+
+ if(digitalRead(ir1)==HIGH)
+  {  
+     while(digitalRead(ir2)==LOW)
+    {
+      if(digitalRead(ir2)==HIGH)
+      {
+        //count=count+1;
+      break;
+      }
+   }
+   count=count+1;
+    //Serial.print("total no. of people inside  =");
+    //Serial.print(count);
+    //Serial.println();
+    Serial.println("ir 1 se ir 2");
+      digitalWrite(led,HIGH);
+  }
+ else if(digitalRead(ir2)==HIGH)
+  {    
+     while(digitalRead(ir1)==LOW)
+    {
+      if(digitalRead(ir1)==HIGH)
+      {
+        count=count-1;
+      break;
+      }
+   }
+     //  Serial.print("total no. of people inside  =");
+    //Serial.print(count);
+    //Serial.println();
+      
+      digitalWrite(led,LOW);
+  }
+
+  // put your main code here, to run repeatedly:
+
+}
---
+++ final.ino
@@ -0,0 +1,94 @@
+  #include <Servo.h>
+  int ir1 = 7;
+  int buz = 8;
+  int led1 = 6;
+  int speaker = 11;
+  int flag1 = 0;
+
+ 
+Servo myservo;
+int pos = 0; 
+int state=0;
+int flag=0;
+
+void setup() {
+  pinMode(ir1,INPUT);
+  pinMode(buz,OUTPUT);
+  pinMode(led1,OUTPUT);
+  pinMode(speaker, OUTPUT);
+
+  myservo.attach(9);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+ delayMicroseconds(1000);
+
+  if(digitalRead(ir1) == HIGH)
+  {
+    while((digitalRead(ir1) == HIGH) && (flag <=3))
+    {
+      flag1++;
+      delay(1000);
+      
+      if(flag1 == 3)
+      break;
+    }
+    if(flag1 == 3)
+    {
+      digitalWrite(speaker, HIGH);
+      delay(2000);
+      digitalWrite(speaker, LOW);
+      delay(1000);
+
+      digitalWrite(led1, HIGH);
+ 
+      digitalWrite(buz,HIGH);
+      delay(1100);
+      digitalWrite(buz, LOW);
+      delay(1100);
+      digitalWrite(buz,HIGH);
+      delay(1100);
+      digitalWrite(buz, LOW);
+      digitalWrite(led1, LOW);
+      //.............................................................
+
+      //............................................
+      flag1 = 0;
+    }
+    }
+    else
+    {
+      flag1 = 0;
+    }
+
+    if(Serial.available()>0)
+      {
+  {
+    delay(400);
+    state=Serial.read();
+    flag=0;
+  }
+      if(state=='0')
+      {
+             myservo.write(0);              
+            
+            
+            if(flag==0)
+            {
+              Serial.println("off");
+              flag=1;
+            }
+      }
+      else if(state=='1')
+      {          
+            myservo.write(90);              
+
+            if(flag==0)
+            {
+              Serial.println("on");flag=1;
+            }
+        }
+  }
+}
